afc7d1fa4b2da94355a0632254e586f72e94580e
thdudgus <87588784+thdudgus@users.noreply.github.com>
2024-09-03 17:49:32 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4044 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n;
+    queue<int> card;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        card.push(i + 1);
+
+    while (card.size() > 1)
+    {
+        card.pop();
+        card.push(card.front());
+        card.pop();
+    }
+
+    printf("%d", card.front());
+
+    return 0;
+}
