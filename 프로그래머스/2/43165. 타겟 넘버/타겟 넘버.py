ef52d2801fccd1a5604803d54969b2b1b02a3013
thdudgus <87588784+thdudgus@users.noreply.github.com>
2024-09-22 02:28:29 +0900

    [level 2] Title: 타겟 넘버, Time: 694.02 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,9 @@
 from itertools import combinations
 def solution(numbers, target):
     numbers = list(map(int, numbers))
-    n = len(numbers)
-    total = 0
     answer = 0
     
-    for i in range(1, n): # 뺄셈의 개수
+    for i in range(1, len(numbers)): # 뺄셈의 개수
         minus = list(combinations(numbers, i))
         for j in range(len(minus)):
             total = sum(numbers) - 2*sum(minus[j])
