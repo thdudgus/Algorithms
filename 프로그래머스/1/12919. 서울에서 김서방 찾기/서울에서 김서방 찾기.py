0936ddf41f08f9cd6f460492a2ca877973b8fb7b
thdudgus <87588784+thdudgus@users.noreply.github.com>
2024-10-06 15:22:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,6 +1,4 @@
 def solution(seoul):
-    k=0
-    for i in seoul:
-        if i == 'Kim':
-            return "김서방은 {}에 있다".format(k)
-        k+=1
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            return "김서방은 {}에 있다".format(i)
