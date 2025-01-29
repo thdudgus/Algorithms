d9d5b7577351cee6db794d75cb89442fc7d0e168
thdudgus <87588784+thdudgus@users.noreply.github.com>
2025-01-29 19:28:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 328 ms, Memory: 35712 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,33 +1,46 @@
-N, M, V = map(int, input().split())
-edge=[[] for _ in range(N+1)]
-visited =[False]*(N+1)
-path1=[V]
-path2=[V]
-for _ in range(M):
-    x,y = map(int, input().split())
-    edge[x].append(y)
-    edge[y].append(x)
-for i in range(1,N+1):
-    edge[i].sort()
-def dfs(k):
-    visited[k]=True
-    for i in edge[k]:
-        if visited[i]==False:
-            path1.append(i)
-            dfs(i)
-def bfs(V):
-    queue=[V]
-    visited2=[False]*(N+1)
-    visited2[V]=True
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    # v에 연결된 모든 노드 순회
+    for i in graph[v]:
+        if not visited[i]: #방문하지 않았으면 해당 노드를 시작 노드로 다시 dfs
+            dfs(graph, i, visited)
+
+from collections import deque
+def bfs(graph, start, visited):
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    queue = deque([start])
+
+    # 큐가 빌 때까지 반복
     while queue:
-        k=queue.pop(0)
-        for i in edge[k]:
-            if visited2[i]==False:
-                visited2[i]=True
-                path2.append(i)
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 쿠에 삽입
+        for i in graph[v]:
+            if not visited[i]:
                 queue.append(i)
-            
-dfs(V)
-bfs(V)
-print(*path1)
-print(*path2)
+                visited[i] = True
+
+n = list(map(int,input().split()))
+graph = []
+for i in range(n[0]+1):
+    graph.append([])
+
+for i in range(n[1]):
+    temp = list(map(int, input().split()))
+    graph[temp[0]].append(temp[1])
+    graph[temp[1]].append(temp[0])
+
+for i in range(len(graph)):
+    graph[i].sort()
+
+visited = [False]*(n[0]+1)
+dfs(graph, n[2], visited)
+print()
+visited = [False]*(n[0]+1)
+bfs(graph, n[2], visited)
